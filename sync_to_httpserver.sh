4f9481e7b6e3b1de9d249328849df88691061d54
suxb201 <sxb201@qq.com>
2019-04-11 20:51:20 +0800

    fix sync.sh

--- sync_to_httpserver.sh
+++ sync_to_httpserver.sh
@@ -1,2 +1,2 @@
-scp -r -P 9002 ./docs/.vuepress/dist/* root@mirrors.oops-sdu.cn:/mirrors/http/
+scp -r -P 9002 ./docs/.vuepress/dist/* root@mirrors.oops-sdu.cn:/mirrors/http/public_html/
 read -n 1 -s -r -p "Press any key to continue"
