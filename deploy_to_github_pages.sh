83a9ff503bc9929fb3fcaeeca0411c1a9f107ff6
Sad Pencil <qh06@qq.com>
2019-04-30 04:02:25 +0800

    also, silent popd and pushd

--- deploy_to_github_pages.sh
+++ deploy_to_github_pages.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
-
-# 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "${DIR}"
+
 yarn build
 
-# 进入生成的文件夹
 pushd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
+# required for custom domain at github pages
 echo 'mirrors.oops-sdu.cn' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
+# publish to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# publish to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/oops-sdu/mirrors.qd.sdu.edu.cn.git master:gh-pages
 
+popd
 popd
